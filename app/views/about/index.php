c401aaa2d08b085fa4875c638a1592c56fc06280
lidyaislamiatiz <lidyaislamiati04@gmail.com>
2019-10-08 11:30:53 +0700

    menambahkan folder css dan js

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,7 @@
+<div class="container">
 
-
-    <h1>About me</h1>
+    <h1 class="mt-5">About me</h1>
+    <img scr="<?= BASEURL; ?>/img/lid.PNG" alt="Lidya Islamiati Z" widht="200" class="rounded-circle">
     <p>halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></P>
+
+ </div>
