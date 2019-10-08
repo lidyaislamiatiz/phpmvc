040851fb85f6477812ed8dabe855ed09e0222838
lidyaislamiatiz <lidyaislamiati04@gmail.com>
2019-10-08 09:25:13 +0700

    menambhakn folder views

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,12 +1,21 @@
 <?php
 
-class About {
-    public function index($nama = 'Lidya' , $pekerjaan = 'mahasiswa')
+class About extends Controller{
+    public function index($nama = 'Lidya' , $pekerjaan = 'mahasiswa', $umur = 32)
     {
-        echo"Halo, nama saya $nama, saya seorang $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     public function page()
     {
-        echo'About/page';
+        $data['judul'] = 'Page';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
