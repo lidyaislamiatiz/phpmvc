d77ae95f5503f5bce7551debcdfdd67b5c348f42
lidyaislamiatiz <lidyaislamiati04@gmail.com>
2019-10-09 21:47:10 +0700

    mengubah foto

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,7 +1,7 @@
 <div class="container">
 
     <h1 class="mt-5">About me</h1>
-    <img src="<?= BASEURL; ?>/img/profile.png" alt="Lidya Islamiati Z" widht="200" class="rounded-circle">
+    <img src="<?= BASEURL; ?>/img/lid.PNG" alt="Lidya Islamiati Z" widht="200" class="rounded-circle">
     <p>halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></P>
 
  </div>
